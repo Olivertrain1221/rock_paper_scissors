b274ee3f7a2a682899a98f6925d3f78313292b3d
Olivertrain1221 <olivertrain1996@gmail.com>
2021-10-25 14:33:23 +0000

    Add basic CSS rules to buttons and modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,32 @@
+// modal container element assignement
 let modalContainer = document.getElementById("modal-container");
 let button = document.getElementById("button-instruction");
-let buttonPlay = document.getElementById("button-play");
+let closeInstructionButton = document.getElementById("close-button")
 
 button.addEventListener("click", openModal);
-modalContainer.addEventListener("click", closeModal);
-buttonPlay.addEventListener("click", openModalPlay)
-modalContainer.addEventListener("click", closeModal);
+// modalContainer.addEventListener("click", closeModal);
+closeInstructionButton.addEventListener("click", closeModal);
 
+// Event listener for whole window to run the modalContainerClick func.
+window.addEventListener("click", modalContainerClick);
+
+// Opens the instruction modal
 function openModal(){
     modalContainer.style.display = "block";
 }
 
-
+// 
 function closeModal(){
     modalContainer.style.display = "none";
+    console.log("testing button")
+}
+// Closes the instructions modal
+function modalContainerClick(e) {
+    if (e.target == modalContainer) {
+        modalContainer.style.display = "none";
+        console.log("testing")
+    }
 }
 
 
 
-
-function openModalPlay(){
-    modalContainerPlay.style.display = "block";
-}
